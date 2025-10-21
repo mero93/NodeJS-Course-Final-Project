bb689443a512b70d158cdaa2fa3dd324486dc568
mero93m <merabkopaleishvili1993@gmail.com>
2025-10-21 18:37:04 +0400

    feat: add swagger, test husky

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME ?? 'Vinyl Store')
+    .setDescription(process.env.APP_DESCRIPTION ?? '')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
