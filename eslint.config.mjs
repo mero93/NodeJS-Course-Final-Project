89f944bc89cabc3c67b3b415f5aa7ecf433515bb
mero93m <merabkopaleishvili1993@gmail.com>
2025-10-21 17:36:46 +0400

    feat: configured es-lint, prettier, test husky pre hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'dist'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -26,9 +26,14 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'indent': ['warn', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['warn', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'semi-spacing': ['error', { before: false, after: true }],
     },
   },
 );
