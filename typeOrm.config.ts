a59a05d96d8c2afbe2fc5441eac5a7a9eeefbcf5
mero93m <merabkopaleishvili1993@gmail.com>
2025-10-29 02:55:15 +0400

    try fix

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,4 +1,3 @@
-import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import { Order } from './src/models/entities/order.entity.js';
@@ -10,23 +9,21 @@ import { ReviewSubscriber } from './src/models/subscribers/review.subscriber.js'
 import { Author } from './src/models/entities/author.entity.js';
 import { RevokedToken } from './src/models/entities/revokedToken.entity.js';
 import { VinylSubscriber } from './src/models/subscribers/vinyl.subscriber.js';
-import { DataSourceOptions } from 'typeorm/browser';
+import { DataSourceOptions } from 'typeorm';
 import { OAuthAccount } from './src/models/entities/oauthAccount.js';
 
 config();
 
-const configService = new ConfigService();
-
-const isProduction = configService.get('NODE_ENV') === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  url: isProduction ? configService.getOrThrow('DATABASE_URL') : undefined,
-  host: !isProduction ? configService.getOrThrow('POSTGRES_HOST') : undefined,
-  port: !isProduction ? configService.getOrThrow<number>('POSTGRES_PORT') : undefined,
-  database: !isProduction ? configService.getOrThrow('POSTGRES_DB') : undefined,
-  username: !isProduction ? configService.getOrThrow('POSTGRES_USER') : undefined,
-  password: !isProduction ? configService.getOrThrow('POSTGRES_PASSWORD') : undefined,
+  url: isProduction ? process.env.DATABASE_URL : undefined,
+  host: !isProduction ? process.env.POSTGRES_HOST : undefined,
+  port: !isProduction ? Number(process.env.POSTGRES_PORT) : undefined,
+  database: !isProduction ? process.env.POSTGRES_DB : undefined,
+  username: !isProduction ? process.env.POSTGRES_USER : undefined,
+  password: !isProduction ? process.env.POSTGRES_PASSWORD : undefined,
   ssl: isProduction ? { rejectUnauthorized: false } : undefined,
   migrations: ['./src/data/migrations/**'],
   entities: [
